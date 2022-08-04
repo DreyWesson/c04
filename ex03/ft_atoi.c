31b8707d350428d934e054c45d473ea43c2c78f3
dreywesson <dreywesson@gmail.com>
2022-08-04 19:24:01 +0100

    atoi with neg sign

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,66 +6,76 @@
 /*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 12:58:15 by doduwole          #+#    #+#             */
-/*   Updated: 2022/08/04 18:12:39 by doduwole         ###   ########.fr       */
+/*   Updated: 2022/08/04 19:23:30 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int ft_atoi(char *str)
+int	ft_is_num(char str)
+{
+	if (str >= 48 && str <= 57)
+		return (1);
+	return (0);
+}
+
+void	ft_str_converter(char str, int *res, int *lock)
+{
+	if (ft_is_num(str) == 1)
+	{
+		*lock = 1;
+		*res = (str - '0') + ((*res) * 10);
+	}
+}
+
+void	ft_signs_and_spaces(char str, int *i, int *sign)
+{
+	if (str == 32)
+		i++;
+	if (str == 43)
+	{
+		*sign = *sign + 1;
+		i++;
+	}
+	if (str == 45)
+	{
+		*sign = *sign - 1;
+		i++;
+	}
+}
+
+int	ft_atoi(char *str)
 {
 	int	i;
 	int	res;
-	int neg;
-	int pos;
+	int	lock;
+	int	sign;
 
 	i = 0;
-	pos = 0;
-	neg = 0;
+	lock = 0;
 	res = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] == 32)
-		{
-			i++;
-			continue ;
-		}
-		if (str[i] == 43)
-		{
-			pos++;
-			i++;
-			continue ;
-		}
-		if (str[i] == 45)
-		{
-			neg++;
-			i++;
-			continue ;
-		}
+		if (lock == 1 && ft_is_num(str[i]) != 1)
+			break ;
+		ft_signs_and_spaces(str[i], &i, &sign);
 		if ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122))
 			break ;
-		if (str[i] >= 48 && str[i] <= 57)
-		{
-			printf("HERE\n");
-			res = (str[i] - '0') + (res * 10);
-		}
+		ft_str_converter(str[i], &res, &lock);
 		i++;
 	}
-	if (neg > pos)
-		printf("-");
+	if (sign < 0 && res != 0)
+		return (-res);
 	if (res != 0)
-	{
-		printf("%i", res);
-		return res;
-	}
-	printf("%i", 0);
+		return (res);
 	return (0);
 }
 
-int main(void)
-{
-	// " ---+--+1234ab567"
-	char strn[] = "str";
-	ft_atoi(strn);
-	return (0);
-}
+// int	main(void)
+// {
+// 	// char strn[] = "1234";
+// 	char strn[] = "str";
+// 	// char strn[] = " ---+--+1234ab567";
+// 	printf("%d",ft_atoi(strn));
+// 	return (0);
+// }
