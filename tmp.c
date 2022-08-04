6a17173cd2962f59bd8bf13bfeb9fd7b23013c92
dreywesson <dreywesson@gmail.com>
2022-08-04 19:19:57 +0100

    atoi with neg sign

--- tmp.c
+++ tmp.c
@@ -16,37 +16,28 @@ void ft_str_converter(char str, unsigned int *res, int *lock)
 	}
 }
 
-int ft_signs_and_spaces(char str,int *i)
+void ft_signs_and_spaces(char str,int *i, int *sign)
 {
-	int pos;
-	int neg;
-
-	pos = 0;
-	neg = 0;
 	if (str == 32)
 		i++;
 	if (str == 43)
 	{
-		pos++;
+		*sign = *sign + 1;
 		i++;
 	}
 	if (str == 45)
 	{
-		neg++;
+		*sign = *sign - 1;
 		i++;
 	}
-	if (neg > pos)
-		return (1);
-	else 
-		return (-1);
 }
 
 int ft_atoi(char *str)
 {
-	int	i;
+	int				i;
 	unsigned int	res;
-	int lock;
-	char sign;
+	int 			lock;
+	int 			sign;
 
 	i = 0;
 	lock = 0;
@@ -55,17 +46,14 @@ int ft_atoi(char *str)
 	{
 		if (lock == 1 && ft_is_num(str[i]) != 1)
 			break ;
-		sign = ft_signs_and_spaces(str[i], &i);
+		ft_signs_and_spaces(str[i], &i,&sign);
 		if ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122))
 			break ;
 		ft_str_converter(str[i], &res, &lock);
 		i++;
 	}
-	if (sign == 1)
-	{
-		res = -res;
-		return (res);
-	}
+	if (sign < 0 && res != 0)
+		return (-res);
 	if (res != 0)
 		return (res);
 	return (0);
@@ -73,9 +61,9 @@ int ft_atoi(char *str)
 
 int main(void)
 {
-	// char strn[] = "1234";
+	char strn[] = "1234";
 	// char strn[] = "str";
-	char strn[] = " ---+--+1234ab567";
+	// char strn[] = " ---+--+1234ab567";
 	printf("%d",ft_atoi(strn));
 	return (0);
 }
